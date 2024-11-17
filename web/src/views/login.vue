82b310a381384b4dc5e63f04a2a578b629ec49fa
happy <984149846@qq.com>
2024-11-17 19:33:16 +0800

    5.11 Vue CLI多环境配置；为axios配置后端域名

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -50,7 +50,7 @@ export default defineComponent({
     });
 
     const sendCode = () => {
-      axios.post("http://localhost:8000/member/member/send-code", {
+      axios.post("/member/member/send-code", {
         mobile: loginForm.mobile
       }).then(response => {
         let data = response.data;
@@ -64,7 +64,7 @@ export default defineComponent({
     };
 
     const login = () => {
-      axios.post("http://localhost:8000/member/member/login", loginForm).then(response => {
+      axios.post("/member/member/login", loginForm).then(response => {
         let data = response.data;
         if (data.success) {
           notification.success({ description: '登录成功！' });
@@ -73,6 +73,7 @@ export default defineComponent({
         }
       })
     };
+
     return {
       loginForm,
       sendCode,
