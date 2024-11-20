58f76b5a0ab96f753527f1668ed6e7db17b90002
happy <984149846@qq.com>
2024-11-20 19:44:03 +0800

    8.9 前端枚举代码生成器

--- member/src/main/java/com/jiawa/train/member/resp/PassengerQueryResp.java
+++ member/src/main/java/com/jiawa/train/member/resp/PassengerQueryResp.java
@@ -1,10 +1,13 @@
 package com.jiawa.train.member.resp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-import java.util.Date;
-import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 
+import java.util.Date;
+
+@Data
 public class PassengerQueryResp {
 
     /**
