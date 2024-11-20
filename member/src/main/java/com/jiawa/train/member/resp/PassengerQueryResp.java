095fe9dbfcc170176edac5057c51aa43023de276
happy <984149846@qq.com>
2024-11-20 10:58:36 +0800

    7.11 解决Long类型精度丢失的问题

--- member/src/main/java/com/jiawa/train/member/resp/PassengerQueryResp.java
+++ member/src/main/java/com/jiawa/train/member/resp/PassengerQueryResp.java
@@ -1,14 +1,18 @@
 package com.jiawa.train.member.resp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 public class PassengerQueryResp {
+    @JsonSerialize(using= ToStringSerializer.class)
     private Long id;
 
+    @JsonSerialize(using= ToStringSerializer.class)
     private Long memberId;
 
     private String name;
