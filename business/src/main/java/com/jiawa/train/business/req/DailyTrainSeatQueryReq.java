8bb96a024882800605182ec442cc2adda58f6156
happy <984149846@qq.com>
2024-11-24 12:52:50 +0800

    11.5 快速生成每日座位数据管理功能

---
+++ business/src/main/java/com/jiawa/train/business/req/DailyTrainSeatQueryReq.java
@@ -0,0 +1,9 @@
+package com.jiawa.train.business.req;
+
+import com.jiawa.train.common.req.PageReq;
+import lombok.Data;
+
+@Data
+public class DailyTrainSeatQueryReq extends PageReq {
+    private String trainCode;
+}
