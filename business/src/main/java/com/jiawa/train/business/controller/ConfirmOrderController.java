a815bd374bd7499abe1887d95017c77282ee2d41
happy <984149846@qq.com>
2024-12-07 12:44:20 +0800

    18.1 增加秒杀令牌表用以维护令牌信息

--- business/src/main/java/com/jiawa/train/business/controller/ConfirmOrderController.java
+++ business/src/main/java/com/jiawa/train/business/controller/ConfirmOrderController.java
@@ -1,10 +1,7 @@
 package com.jiawa.train.business.controller;
 
-import com.alibaba.csp.sentinel.annotation.SentinelResource;
-import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.jiawa.train.business.req.ConfirmOrderDoReq;
 import com.jiawa.train.business.service.ConfirmOrderService;
-import com.jiawa.train.common.exception.BusinessExceptionEnum;
 import com.jiawa.train.common.resp.CommonResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
@@ -24,7 +21,7 @@ public class ConfirmOrderController {
     @Resource
     private ConfirmOrderService confirmOrderService;
 
-    @SentinelResource(value = "confirmOrderDo", blockHandler = "doConfirmBlock")
+//    @SentinelResource(value = "confirmOrderDo", blockHandler = "doConfirmBlock")
     @PostMapping("/do")
     public CommonResp<Object> doConfirm(@Valid @RequestBody ConfirmOrderDoReq req) {
         confirmOrderService.doConfirm(req);
@@ -32,17 +29,17 @@ public class ConfirmOrderController {
     }
 
 
-    /** 降级方法，需包含限流方法的所有参数和BlockException参数，且返回值要保持一致
-     * @param req
-     * @param e
-     */
-    public CommonResp<Object> doConfirmBlock(ConfirmOrderDoReq req, BlockException e) {
-        LOG.info("ConfirmOrderController购票请求被限流：{}", req);
-        // throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION);
-        CommonResp<Object> commonResp = new CommonResp<>();
-        commonResp.setSuccess(false);
-        commonResp.setMessage(BusinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION.getDesc());
-        return commonResp;
-
-    }
+//    /** 降级方法，需包含限流方法的所有参数和BlockException参数，且返回值要保持一致
+//     * @param req
+//     * @param e
+//     */
+//    public CommonResp<Object> doConfirmBlock(ConfirmOrderDoReq req, BlockException e) {
+//        LOG.info("ConfirmOrderController购票请求被限流：{}", req);
+//        // throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION);
+//        CommonResp<Object> commonResp = new CommonResp<>();
+//        commonResp.setSuccess(false);
+//        commonResp.setMessage(BusinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION.getDesc());
+//        return commonResp;
+//
+//    }
 }
